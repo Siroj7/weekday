bf3ff0388b2ad03d7b41942d44df73cd8291f1bb
Siroj <rustamovsiroj5@gmail.com>
2022-10-11 17:20:12 +0500

    Project added

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let weekDay = 'seshanba';
+
+switch (weekDay) {
+    case "dushanba":
+        console.log('seshanba');
+        break
+    case 'seshanba':
+        console.log('chorshanba');
+        break
+    case 'chorshanba':
+        console.log("payshanba");
+        break
+    case 'payshanba':
+        console.log("juma");
+        break
+    case 'juma':
+        console.log("shanba");
+        break
+    case 'shanba':
+        console.log("yakshanba");
+        break
+    default:
+        console.log('Bunaqa hafta kuni mavjud emas')
+}
